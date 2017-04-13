19fa6c9e0eac74928f1cd8c9bf67e2b467f31914
Brandon Robyn <brobyn1992@gmail.com>
2017-04-12 22:58:06 -0500

    begin moving some repetitive styling into their own files

--- Entities/Constants/Constants.cs
+++ Entities/Constants/Constants.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Entities.Constants
 {
 
 
   #region FileSuffixes
-  public class FileSuffixes
+  public static class FileSuffixes
   {
     public const string ImageSuffix = ".png";
     public const string PieceSuffix = "Piece.png";
@@ -18,18 +20,66 @@ namespace Entities.Constants
 
 
   #region FolderPaths
-  public class FolderPaths
+  public static class FolderPaths
   {
     public const string FireFolder = "/PokemonChess;component/Images/FirePokemon/";
     public const string GrassFolder = "/PokemonChess;component/Images/GrassPokemon/";
     public const string PoisonFolder = "/PokemonChess;component/Images/PoisonPokemon/";
     public const string WaterFolder = "/PokemonChess;component/Images/WaterPokemon/";
+
+    #region BlackPieces
+    public const string BlackKingPath = "pack://application:,,,/Images/ChessPieces/BlackKing.PNG";
+    public static ImageSource BlackKingImageSource { get { return StringToImageSource(BlackKingPath); } }
+
+    public const string BlackQueenPath = "pack://application:,,,/Images/ChessPieces/BlackQueen.PNG";
+    public static ImageSource BlackQueenImageSource { get { return StringToImageSource(BlackQueenPath); } }
+
+    public const string BlackRookPath = "pack://application:,,,/Images/ChessPieces/BlackRook.PNG";
+    public static ImageSource BlackRookImageSource { get { return StringToImageSource(BlackRookPath); } }
+
+    public const string BlackBishopPath = "pack://application:,,,/Images/ChessPieces/BlackBishop.PNG";
+    public static ImageSource BlackBishopImageSource { get { return StringToImageSource(BlackBishopPath); } }
+
+    public const string BlackKnightPath = "pack://application:,,,/Images/ChessPieces/BlackKnight.PNG";
+    public static ImageSource BlackKnightImageSource { get { return StringToImageSource(BlackKnightPath); } }
+
+    public const string BlackPawnPath = "pack://application:,,,/Images/ChessPieces/BlackPawn.PNG";
+    public static ImageSource BlackPawnImageSource { get { return StringToImageSource(BlackPawnPath); } }
+    #endregion
+
+
+    #region WhitePieces
+    public const string WhiteKingPath = "pack://application:,,,/Images/ChessPieces/WhiteKing.PNG";
+    public static ImageSource WhiteKingImageSource { get { return StringToImageSource(WhiteKingPath); } }
+
+    public const string WhiteQueenPath = "pack://application:,,,/Images/ChessPieces/WhiteQueen.PNG";
+    public static ImageSource WhiteQueenImageSource { get { return StringToImageSource(WhiteQueenPath); } }
+
+    public const string WhiteRookPath = "pack://application:,,,/Images/ChessPieces/WhiteRook.PNG";
+    public static ImageSource WhiteRookImageSource { get { return StringToImageSource(WhiteRookPath); } }
+
+    public const string WhiteBishopPath = "pack://application:,,,/Images/ChessPieces/WhiteBishop.PNG";
+    public static ImageSource WhiteBishopImageSource { get { return StringToImageSource(WhiteBishopPath); } }
+
+    public const string WhiteKnightPath = "pack://application:,,,/Images/ChessPieces/WhiteKnight.PNG";
+    public static ImageSource WhiteKnightImageSource { get { return StringToImageSource(WhiteKnightPath); } }
+
+    public const string WhitePawnPath = "pack://application:,,,/Images/ChessPieces/WhitePawn.PNG";
+    public static ImageSource WhitePawnImageSource { get { return StringToImageSource(WhitePawnPath); } }
+    #endregion
+
+
+    public static ImageSource StringToImageSource(string path)
+    {
+      var converter = new ImageSourceConverter();
+      return (ImageSource)converter.ConvertFromString(path);
+    }
   }
   #endregion
 
 
   #region PieceTypes
-  public class PieceTypes
+  public static class PieceTypes
   {
     public const string King = "King";
     public const string Queen = "Queen";
@@ -42,7 +92,7 @@ namespace Entities.Constants
 
 
   #region PokemonNames
-  public class PokemonNames
+  public static class PokemonNames
   {
     // Fire
     public const string Arcanine = "Arcanine";
